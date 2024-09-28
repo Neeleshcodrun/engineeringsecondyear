215986a1a3d5d24bacb5603e456041a8c85aa2c0
Neeleshcodrun <156096773+Neeleshcodrun@users.noreply.github.com>
2024-09-28 20:35:03 +0530

    beginjs
    learn js

---
+++ register.js
@@ -0,0 +1,54 @@
+const account_Id =13487
+let account_Email="neelesh@google.com"
+var account_Password = "74934"
+account_City ="mathura"
+let account_State;
+
+//account_Id=2 //not allowed
+
+
+
+account_Email="hitesg@goohl.com"
+account_Password="6487"
+account_City="goverdhan"
+
+console.log(account_Id);
+
+/*
+prefer not to use var 
+Because of issue in block scope and functional scope
+*/
+
+console.table([account_Id,account_Email,account_Password,account_City,account_State])
+
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)---we are using nodejs not browser
+
+
+console.log(3+3);
+
+
+//code read ability should be high 
+console.log("neelesh")
+
+//Data type Start
+
+let name="neelesh"// String data type
+let age=18 //numbers data type
+let isloggedIn=true//  boolean data type
+let state;
+
+
+// number=>2 to power 53
+//bigint
+///string=>""
+// boolean => true/false
+//null (type hai) ==>standlone value
+// undefined =>  (it means value is not assigned)
+// symbol => it should be used in uniqueness 
+
+//object
+
+console.log(typeof  null);
+console.log(typeof undefined);//undefined is a type of undefined
